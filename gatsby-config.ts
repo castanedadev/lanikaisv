0858145bf94cb34fea0defabb7cb48e6a49c6b7e
Ricardo Castaneda <rickcastaneda@outlook.com>
2025-05-04 08:32:53 -0600

    Integrate gatsby with graphql & add *.mdx blog posts (#3)
    * chore: configs to integrate data layer
    
    * chore: add sample blog posts
    
    * chore: add new page/component
    
    * chore: update main layout
    
    * include plugin gatsby-source-filesystem
    
    * update existing pages

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -2,7 +2,10 @@ import type { GatsbyConfig } from "gatsby"
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    siteUrl: `https://www.yourdomain.tld`,
+    description: 'Lanikai SV is a jewelry brand that specializes in selling high-quality, handcrafted jewelry. Our mission is to provide our customers with unique and beautiful pieces that they can cherish for a lifetime.',
+    siteUrl: `https://www.lanikaisv.com`,
+    title: 'Lanikai SV',
+    twitterUsername: `@lanikaisv`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
@@ -11,6 +14,13 @@ const config: GatsbyConfig = {
   plugins: [
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/src/blog`,
+      }
+    },
   ],
 }
 
