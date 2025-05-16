0bc636bc7118ae03937a9a3be10df350e6e361e2
Ricardo Castaneda <rickcastaneda@outlook.com>
2025-05-16 00:48:03 -0600

    chore(deps): update missing plugin

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -8,9 +8,6 @@ const config: GatsbyConfig = {
 		title: "Lanikai SV",
 		twitterUsername: "@lanikaisv",
 	},
-	// More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
-	// If you use VSCode you can also use the GraphQL plugin
-	// Learn more at: https://gatsby.dev/graphql-typegen
 	graphqlTypegen: true,
 	plugins: [
 		"gatsby-plugin-image",
@@ -23,20 +20,32 @@ const config: GatsbyConfig = {
 				path: `${__dirname}/src/blog`,
 			},
 		},
-		"gatsby-plugin-mdx",
 		{
-			resolve: "gatsby-transformer-remark",
+			resolve: "gatsby-plugin-mdx",
 			options: {
-				plugins: ["gatsby-remark-autolink-headers"],
-				options: {
-					offsetY: "100",
-					icon: `<svg aria-hidden="true" height="20" version="1.1" viewBox="0 0 16 16" width="20"><path fill-rule="evenodd" d="M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z"></path></svg>`,
-					className: "custom-class",
-					maintainCase: true,
-					removeAccents: true,
-					isIconAfterHeader: true,
-					elements: ["h1", "h4"],
-				},
+				extensions: [".mdx", ".md"],
+				gatsbyRemarkPlugins: [
+					{
+						resolve: "gatsby-remark-images",
+						options: {
+							maxWidth: 800,
+							showCaptions: true,
+							withWebp: true,
+						},
+					},
+					{
+						resolve: "gatsby-remark-autolink-headers",
+						options: {
+							offsetY: 100,
+							icon: `<svg aria-hidden="true" height="20" viewBox="0 0 16 16" width="20"><path fill-rule="evenodd" d="M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z"></path></svg>`,
+							className: "custom-class",
+							maintainCase: true,
+							removeAccents: true,
+							isIconAfterHeader: true,
+							elements: ["h1", "h4"],
+						},
+					},
+				],
 			},
 		},
 	],
