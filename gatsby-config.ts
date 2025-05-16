1c1230c6f3d64ecb31a1e0f34b3b1c9ca7906031
Ricardo Castaneda <rickcastaneda@outlook.com>
2025-05-16 00:27:47 -0600

    chore(deps): include plugin to process image

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -14,6 +14,7 @@ const config: GatsbyConfig = {
 	graphqlTypegen: true,
 	plugins: [
 		"gatsby-plugin-image",
+		"gatsby-transformer-sharp",
 		"gatsby-plugin-sharp",
 		{
 			resolve: "gatsby-source-filesystem",
